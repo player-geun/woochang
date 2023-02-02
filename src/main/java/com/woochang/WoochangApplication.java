627e78344b7cf16e5827335634a9be9d244f69b9
player-geun <player_geun@naver.com>
2023-02-02 19:19:34 +0900

    init : 프로젝트 초기화(#1)
    초기 프로젝트 설정

---
+++ src/main/java/com/woochang/WoochangApplication.java
@@ -0,0 +1,13 @@
+package com.woochang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoochangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoochangApplication.class, args);
+	}
+
+}
